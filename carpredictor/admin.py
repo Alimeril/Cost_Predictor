1ef8c914971087cbe53e2ecdb1fe2be94f26cd99
Ali Soltanmanesh <alimeril2@gmail.com>
2024-06-06 01:13:41 +0330

    debugging admin file

--- carpredictor/admin.py
+++ carpredictor/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import Car
 
 # Register your models here.
-class BookAdmin(admin.ModelAdmin):
+class CarAdmin(admin.ModelAdmin):
   list_display = ("name", "year", "description","km","price","created")
   
 admin.site.register(Car, CarAdmin)

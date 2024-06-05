7910407167407f1e5835dddde9c9cd14a0a7e790
Ali Soltanmanesh <alimeril2@gmail.com>
2024-06-05 23:36:50 +0330

    Creating CarList view and template

--- carpredictor/urls.py
+++ carpredictor/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'carpredictor'
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('CarList/', views.CarList.as_view(), name='CarList')
 ]
--- carpredictor/views.py
+++ carpredictor/views.py
@@ -1,5 +1,17 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views import generic
+from .models import Car
 
-# Create your views here.
 def home(request):
     return render(request, 'carpredictor/index.html')
+
+class CarList(generic.ListView):
+    template_name = 'carpredictor/carlist.html'
+    context_object_name = 'car_list'
+    paginate_by = 100
+
+    def get_queryset(self):
+        car_list = Car.objects.order_by('created')
+        return car_list
